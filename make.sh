6640db67b2c1057276cc40f1892b6682b9c0955e
Greg Lamb <email@greglamb.me>
2017-10-10 11:34:11 -0700

    WSL improvements

--- make.sh
+++ make.sh
@@ -3,6 +3,13 @@
 # Use:
 # curl -s https://raw.githubusercontent.com/greglamb/laradock/master/make.sh | bash -s
 
+# For WSL:
+# sudo ln -s /mnt/c/Program\ Files/Docker/Docker/resources/bin/docker-compose.exe /usr/local/bin/docker-compose
+# sudo ln -s /mnt/c/Program\ Files/Docker/Docker/resources/bin/docker.exe /usr/local/bin/docker
+
+containers="$@"
+[ -z "$containers" ] && containers="nginx mysql php-fpm"
+
 contname=$(basename $PWD)_$(date +%Y%m%d%H%M%S)
 git submodule add https://github.com/greglamb/laradock.git laradock
 
@@ -11,7 +18,7 @@ laradock/envtobash.sh laradock/.env > laradock/env.sh
 cat >laradock-up.sh <<EOL
 #!/bin/bash
 source laradock/env.sh
-docker-compose -f laradock/docker-compose.yml -p ${contname} up -d nginx mysql php-fpm memcached
+docker-compose -f laradock/docker-compose.yml -p ${contname} up -d ${containers}
 EOL
 
 cat >laradock-down.sh <<EOL
